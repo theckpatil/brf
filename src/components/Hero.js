4510c8bc16d8294d144c4b9e7b4f7f02cdd43bd3
Chandrakant Patil <patilchandrakant758@gmail.com>
2024-12-30 16:51:15 +0530

    add click handler to prevent default scrolling behavior in Capabilities, Hero, and Process components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 function Hero() {
+    const handleCardClick = (e) => {
+        e.preventDefault();  // Prevent default scrolling behavior
+      };
+      
     return (
         <section className="hero">
             <div className="hero-content">
-                <div className="t-shirt-container">
+                <div className="t-shirt-container" onClick={handleCardClick}>
                     <img src="../assets/img/T-shirt/T-shirt-svg.svg" alt="T-shirt" className="t-shirt-right" />
                 </div>
                 <h1><span style={{color: 'black'}}>BR</span> Fashion</h1>
--- src/components/Process.js
+++ src/components/Process.js
@@ -4,7 +4,6 @@ function Process() {
     const handleCardClick = (e) => {
         e.preventDefault();  // Prevent default scrolling behavior
       };
-      
 
   return (
     <section class="process" id="process">
@@ -12,7 +11,7 @@ function Process() {
             <h2>Manufacturing Process</h2>
             <p class="section-description">From concept to creation, explore how we craft garments with precision and care.</p>
             <div class="process-grid">
-                {/* <!-- Card 1: Design --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/Design.png" alt="Design Icon"/>
@@ -20,7 +19,7 @@ function Process() {
                     <h3>Design</h3>
                     <p>Turning your vision into designs.</p>
                 </div>
-                {/* <!-- Card 2: Sample --> */}
+                
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/sample.png" alt="Sample Icon"/>
@@ -28,7 +27,7 @@ function Process() {
                     <h3>Sample</h3>
                     <p>Creating prototypes to perfect every detail.</p>
                 </div>
-                {/* <!-- Card 6: Embroidery --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/order.png" alt="Embroidery Icon"/>
@@ -36,7 +35,7 @@ function Process() {
                     <h3>Order Confirmation</h3>
                     <p>Creating intricate embroidery patterns with precision.</p>
                 </div>
-                {/* <!-- Card 7: Washing --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/fab-2.png" alt="Washing Icon"/>
@@ -44,7 +43,7 @@ function Process() {
                     <h3>Fabric Processing</h3>
                     <p>Premium fabric, expertly treated for perfect garments.</p>
                 </div>
-                {/* <!-- Card 3: Cutting --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/cut.png" alt="Cutting Icon"/>
@@ -52,7 +51,7 @@ function Process() {
                     <h3>Cutting</h3>
                     <p>Precision cutting using advanced machinery</p>
                 </div>
-                {/* <!-- Card 5: Printing --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/painting.png" alt="Printing Icon"/>
@@ -60,7 +59,7 @@ function Process() {
                     <h3>Printing</h3>
                     <p>Applying high-quality prints for your custom design.</p>
                 </div>
-                {/* <!-- Card 4: Stitching --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/sewing-machine.png" alt="Stitching Icon"/>
@@ -68,7 +67,7 @@ function Process() {
                     <h3>Stitching</h3>
                     <p>Expert stitching to assemble your garment seamlessly.</p>
                 </div>
-                {/* <!-- Card 9: Quality Control --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/garment-check.png" alt="Quality Control Icon"/>
@@ -76,7 +75,7 @@ function Process() {
                     <h3>Garment Checking</h3>
                     <p>Thorough inspection to maintain high standards.</p>
                 </div>
-                {/* <!-- Card 8: Ironing --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/Iron.png" alt="Ironing Icon"/>
@@ -84,7 +83,7 @@ function Process() {
                     <h3>Ironing</h3>
                     <p>Ensuring your garment looks crisp and polished.</p>
                 </div>
-                {/* <!-- Card 10: Packaging --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/packing.png" alt="Packaging Icon"/>
@@ -92,7 +91,7 @@ function Process() {
                     <h3>Packaging</h3>
                     <p>Neatly packaging the garment for delivery.</p>
                 </div>
-                {/* <!-- Card 12: Final Delivery --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/final-check.png" alt="Final Delivery Icon"/>
@@ -100,7 +99,7 @@ function Process() {
                     <h3>Final Inspection</h3>
                     <p>Final inspections guarantee exceptional quality in every garment.</p>
                 </div>
-                {/* <!-- Card 11: Shipping --> */}
+
                 <div class="process-card" onClick={handleCardClick}>
                     <div class="icon-container">
                         <img src="assets/img/process/Shipping.png" alt="Shipping Icon"/>
